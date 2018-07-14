a95a63c7d178e335518315ed9b8a64f977425f70
SrMoffat <ngigemoffat@gmail.com>
2018-07-14 15:24:56 +0300

    [Chore#159054089] Setup application factory and basic custom commands [run][test] using flask-script [finishes#159054089]

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,1 +1,11 @@
 #main __init__.py
+from flask import Flask 
+
+from .config import app_configs
+
+
+def create_app(config_name): 
+    app = Flask(__name__)
+    app.config.from_object(app_configs[config_name])
+
+    return app
---
+++ manage.py
@@ -0,0 +1,30 @@
+#manage.py 
+import os 
+import unittest
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from app.main import create_app, app_configs
+
+app = create_app('test')
+
+manager = Manager(app)
+
+@manager.command
+def run(): 
+    app.run()
+
+@manager.command
+def test(): 
+    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if result.wasSuccesful(): 
+        return 0
+    return 1
+
+if __name__ == '__main__': 
+    manager.run()
+
+
