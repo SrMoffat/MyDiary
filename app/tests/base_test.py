646d3417ac9b5d328160615e224c7784596a6b60
SrMoffat <ngigemoffat@gmail.com>
2018-07-15 01:10:52 +0300

    [Chore#159023457 #159023473 #159023689 #159023888] Added the tests for the entries operations [finishes #159023888 #159023689 #159023473 #159023457]

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -1,23 +1,24 @@
 #base_test.py
-import json
-from datetime import datetime
 from flask_testing import TestCase
 
 from manage import app
 from app.main.model.entries import mock_db, MockDB
 
 
-class BaseTestCase(TestCase):
+
+class BaseTestCase(TestCase):  
+    def create_app(self): 
+        app.config.from_object('app.main.config.TestingConfig') 
+        return app     
     
     def setUp(self):
-        self.app = app.config.from_object('app.main.config.TestingConfig')
+        self.app = app
+        self.client = self.app.test_client
         self.app.testing = True
-        self.app = self.app.test_client()
         self.entry = {
             'id' : 1,
             'title': 'My Day on the Moon',
             'content': 'So today was a very interesting day on the because I had 2 chicken drumsticks on the moon',
-            'posted on' : datetime.utcnow()
         }
 
         MockDB.entries.append(self.entry)
