ac694f5da807d43f82b510ec44b67aca34385a57
kelvinrandu <randukelvin@gmail.com>
2018-08-01 12:55:28 +0300

    [Chore#159268098 #159267774 #159268047 #159268208 #159268147] Added the refactored tests for the endpoints [finishes #159268098 #159267774 #159268047 #159268208 #159268147]

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -3,7 +3,7 @@ import unittest
 import json 
 
 from .base_test import BaseTestCase
-from app.tests.helper_methods import register_user, login_user
+from .helper_methods import register_user, login_user
 
 class TestAuth(BaseTestCase):
     """
@@ -12,14 +12,20 @@ class TestAuth(BaseTestCase):
     def test_user_signup(self):
         with self.client:
             response = register_user(self)
+            result = json.loads(response.data)
             self.assertEqual(response.status_code, 201)
+            self.assertIn("User registered!", result[u"message"])
+            self.assertIn("success!", result[u"status"])
     def test_double_registration(self):
         with self.client:
             response = register_user(self)
             self.assertEqual(response.status_code, 201)
         with self.client:
             response = register_user(self)
+            result = json.loads(response.data)
             self.assertEqual(response.status_code, 409)
+            self.assertIn("Username exists!", result[u"message"])
+            self.assertIn("failed!", result[u"status"])
     def test_invalid_username(self):
         with self.client:
             response = self.client.post('/api/v2/auth/signup',
@@ -29,7 +35,10 @@ class TestAuth(BaseTestCase):
                                         password='mypassword')),
                                         content_type='application/json'
                                         )
+            result = json.loads(response.data)
             self.assertEqual(response.status_code, 400)
+            self.assertIn("Invalid characters in username!", result[u"error"])
+            self.assertIn("failed!", result[u"status"])
     def test_invalid_email(self):
         with self.client:
             response = self.client.post('/api/v2/auth/signup',
@@ -38,7 +47,10 @@ class TestAuth(BaseTestCase):
                                         email='mymail@mail',
                                         password='mypassword')),
                                         content_type='application/json')
+            result = json.loads(response.data)
             self.assertEqual(response.status_code, 400)
+            self.assertIn("Invalid email!", result[u"error"])
+            self.assertIn("failed!", result[u"status"])
     def test_weak_password(self):
         with self.client:
             response = self.client.post('/api/v2/auth/signup',
@@ -47,13 +59,18 @@ class TestAuth(BaseTestCase):
                                         email='mymail@mail.com',
                                         password='myp')),
                                         content_type='application/json')
+            result = json.loads(response.data)
             self.assertEqual(response.status_code, 400)
+            self.assertIn("Password too weak!", result[u"error"])
+            self.assertIn("failed!", result[u"status"])
     def test_user_login(self):
         with self.client:
             response = register_user(self)
             self.assertEqual(response.status_code, 201)
             response = login_user(self)
-            self.assertEqual(response.status_code, 200)    
+            result = json.loads(response.data)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("You are now logged in!" , result[u"message"])   
     def test_invalid_login_creds(self):
         with self.client:
             response = register_user(self)
@@ -63,9 +80,16 @@ class TestAuth(BaseTestCase):
                                         username='TestUser',
                                         password='TestUserpas')),
                                         content_type='application/json')
+            result = json.loads(response.data)
             self.assertEqual(response.status_code, 401)
+            self.assertIn("failed!",result[u"status"])
+            self.assertIn("Incorrect password!",result[u"message"])
     def test_login_unregistered_user(self):
         with self.client:
             response = login_user(self)
+            result = json.loads(response.data)
             self.assertEqual(response.status_code, 401)
+            self.assertIn("failed!",result[u"status"])
+            self.assertIn("Invalid credentials!",result[u"error"])
+
    
--- app/tests/test_entries.py
+++ app/tests/test_entries.py
@@ -1,1 +1,130 @@
 # test_entries.py
+import json
+import unittest
+
+from .base_test import BaseTestCase
+from .helper_methods import register_user, login_user
+
+class TestEntries(BaseTestCase):
+    """
+    Test class for entry operations
+    """
+    def test_add_entry(self):
+        with self.client:
+            response = register_user(self)
+            self.assertEqual(response.status_code, 201)
+            response = login_user(self)
+            self.assertEqual(response.status_code, 200)
+            result = json.loads(response.data)            
+            self.assertIn("You are now logged in!", result["message"])
+            self.assertIn("success!", result["status"])
+            self.assertIn("token", result)
+            response = self.client.post("api/v2/entries",
+                                        headers={
+                                            "token":result["token"],
+                                            "content-type":"application/json"
+                                        },
+                                        data=self.post_entry)
+            result = json.loads(response.data)
+            self.assertEqual(response.status_code, 201)
+            self.assertIn("Entry added!", result[u"message"])
+            self.assertIn("success!", result[u"status"])
+
+    def test_get_all_entries(self):
+        with self.client:
+            response = register_user(self)
+            self.assertEqual(response.status_code, 201)
+            response = login_user(self)
+            self.assertEqual(response.status_code, 200)
+            result = json.loads(response.data)            
+            self.assertIn("You are now logged in!", result["message"])
+            self.assertIn("success!", result["status"])
+            self.assertIn("token", result)
+            token = result["token"]
+            response = self.client.post("api/v2/entries",
+                                        headers={
+                                            "token":token,
+                                            "content-type":"application/json"
+                                        },
+                                        data=self.post_entry)
+            result = json.loads(response.data)
+            self.assertEqual(response.status_code, 201)
+            self.assertIn("Entry added!", result[u"message"])
+            self.assertIn("success!", result[u"status"])
+            response = self.client.get("api/v2/entries",
+                                        headers={
+                                            "token":token,
+                                            "content-type":"application/json"
+                                        })
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("Test entry post", response.data)
+
+    def test_get_entry_by_id(self):
+        with self.client:
+            response = register_user(self)
+            self.assertEqual(response.status_code, 201)
+            response = login_user(self)
+            self.assertEqual(response.status_code, 200)
+            result = json.loads(response.data)
+            self.assertIn("token", result)
+            token = result["token"]
+            response = self.client.post("api/v2/entries",
+                                        headers={
+                                            "token":token,
+                                            "content-type":"application/json"
+                                        },
+                                        data=self.post_entry)
+            self.assertEqual(response.status_code, 201)            
+            response = self.client.get("api/v2/entries/1",
+                                        headers={
+                                            "token":token,
+                                            "content-type":"application/json"
+                                        })
+            self.assertEqual(response.status_code, 200)
+
+    def test_entry_modification(self):
+        with self.client:
+            response = register_user(self)
+            self.assertEqual(response.status_code, 201)
+            response = login_user(self)
+            self.assertEqual(response.status_code, 200)
+            result = json.loads(response.data)
+            self.assertIn("token", result)
+            token = result["token"]
+            response = self.client.post("api/v2/entries",
+                                        headers={
+                                            "token":token,
+                                            "content-type":"application/json"
+                                        },
+                                        data=self.post_entry)
+            self.assertEqual(response.status_code, 201)
+            response = self.client.put("api/v2/entries/1",
+                                        headers={
+                                            "token":token,
+                                            "content-type":"application/json"
+                                        },
+                                        data=self.update_entry)
+            self.assertEqual(response.status_code, 200)
+
+    def test_delete_entry(self):
+        with self.client:
+            response = register_user(self)
+            self.assertEqual(response.status_code, 201)
+            response = login_user(self)
+            self.assertEqual(response.status_code, 200)
+            result = json.loads(response.data)
+            token = result["token"]
+            response = self.client.post("api/v2/entries",
+                                        headers={
+                                            "token":token,
+                                            "content-type":"application/json"
+                                        },
+                                        data=self.post_entry)
+            self.assertEqual(response.status_code, 201)
+            response = self.client.delete("api/v2/entries/1",
+                                        headers={
+                                            "token":token,
+                                            "content-type":"application/json"
+                                        })
+            self.assertEqual(response.status_code, 200)
+            
